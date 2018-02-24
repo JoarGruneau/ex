eb43559873769032fd43a793d83cdf00b483d111
Joar Gruneau <joar@gruneau.se>
2018-02-24 14:22:38 +0000

    u p

--- code/UGAN/main.py
+++ code/UGAN/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     data_provider = image_util.ImageDataProvider("images/*.png", net.offset//2, data_suffix='.png', mask_suffix='_mask.png')
     trainer = unet.Trainer(net, batch_size=1, optimizer='momentum', opt_kwargs={'momentum': 0.9})
     path = trainer.train(data_provider, "train/", dropout=0.5,
-                         training_iters=1271, epochs=25, display_step=1,  restore=False)
+                         training_iters=1271, epochs=25, display_step=30,  restore=False)
     x_test = a._load_file("images/00000000.png")
     x_test = a._process_data([x_test])
     prediction = net.predict(path, x_test)
