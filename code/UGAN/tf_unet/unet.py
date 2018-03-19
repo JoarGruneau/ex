3308f623a9a039a88184d16327318bd4c226b319
Joar Gruneau <joar@gruneau.se>
2018-03-20 00:29:00 +0100

    weighted loss function

--- code/UGAN/tf_unet/unet.py
+++ code/UGAN/tf_unet/unet.py
@@ -243,7 +243,7 @@ class Unet(object):
             else:
                 # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits, 
                 #                                                               labels=flat_labels))
-                # loss_map = tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits, 
+                loss_map = tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits, 
                 #                                                               labels=flat_labels)
                 # weight = (tf.cast(tf.size(loss_map[..., 1]), tf.float32)-tf.reduce_sum(loss_map[..., 1]))/tf.reduce_sum(loss_map[..., 1])
                 loss=tf.reduce_sum(loss_map[..., 0])+tf.reduce_sum(loss_map[..., 0])*50.0
