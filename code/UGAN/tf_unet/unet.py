d033d3c47f1bd019aca3d35d2a170921b39d6523
Joar Gruneau <joar@gruneau.se>
2018-03-23 14:06:45 +0100

    small gan test

--- code/UGAN/tf_unet/unet.py
+++ code/UGAN/tf_unet/unet.py
@@ -214,39 +214,40 @@ class Ugan(object):
         self.discriminator_cost = real_cost + fake_cost
         self.fake_prob = 1.0-tf.reduce_mean(tf.sigmoid(fake_logits))
         self.real_prob = tf.reduce_mean(tf.sigmoid(real_logits))
-        self.generator_cost=self.bce_loss + 0.1*tf.reduce_mean(
+        self.generator_fake_cost = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(logits=fake_logits, labels=tf.ones_like(fake_logits)))
+        self.generator_cost=self.bce_loss + 0.1*self.generator_fake_cost
 
-        
+
     def _get_cost(self, logits, cost_name, cost_kwargs):
         """
         Constructs the cost function, either cross_entropy, weighted cross_entropy or dice_coefficient.
-        Optional arguments are: 
+        Optional arguments are:
         class_weights: weights for the different classes in case of multi-class imbalance
         regularizer: power of the L2 regularizers added to the loss function
         """
-        
+
         flat_logits = tf.reshape(logits, [-1, self.n_class])
         flat_labels = tf.reshape(self.y, [-1, self.n_class])
         if cost_name == "cross_entropy":
             class_weights = cost_kwargs.pop("class_weights", None)
-            
+
             if class_weights is not None:
                 class_weights = tf.constant(np.array(class_weights, dtype=np.float32))
-        
+
                 weight_map = tf.multiply(flat_labels, class_weights)
                 weight_map = tf.reduce_sum(weight_map, axis=1)
-        
+
                 loss_map = tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits,
                                                                    labels=flat_labels)
                 weighted_loss = tf.multiply(loss_map, weight_map)
-        
+
                 loss = tf.reduce_mean(weighted_loss)
-                
+
             else:
-                loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits, 
+                loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits,
                                                                               labels=flat_labels))
-                # loss_map = tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits, 
+                # loss_map = tf.nn.softmax_cross_entropy_with_logits(logits=flat_logits,
                 #                                                                labels=flat_labels)
                 # weight = (1000000.0-tf.reduce_sum(loss_map[..., 1]))/tf.reduce_sum(loss_map[..., 1])
                 # loss = (tf.reduce_sum(loss_map[..., 0])+tf.reduce_sum(loss_map[..., 1]*weight))/1000000.0
@@ -273,7 +274,7 @@ class Ugan(object):
             loss=1+pred_loss
             # loss = tf.cond(sum_labels_map > 0, lambda: 1.0 - tf.div(inter,union + eps), lambda: 0.0)
 
-            
+
         else:
             raise ValueError("Unknown cost function: "%cost_name)
 
@@ -281,51 +282,51 @@ class Ugan(object):
         if regularizer is not None:
             regularizers = sum([tf.nn.l2_loss(variable) for variable in self.generator_variables])
             loss += (regularizer * regularizers)
-            
+
         return loss, logits
 
     def predict(self, model_path, x_test):
         """
         Uses the model to create a prediction for the given data
-        
+
         :param model_path: path to the model checkpoint to restore
         :param x_test: Data to predict on. Shape [n, nx, ny, channels]
-        :returns prediction: The unet prediction Shape [n, px, py, labels] (px=nx-self.offset/2) 
+        :returns prediction: The unet prediction Shape [n, px, py, labels] (px=nx-self.offset/2)
         """
-        
+
         init = tf.global_variables_initializer()
         with tf.Session() as sess:
             # Initialize variables
             sess.run(init)
-        
+
             # Restore model weights from previously saved model
             self.restore(sess, model_path)
-            
+
             y_dummy = np.empty((x_test.shape[0], x_test.shape[1], x_test.shape[2], self.n_class))
             prediction = sess.run(self.predicter, feed_dict={self.x: x_test, self.y: y_dummy, self.keep_prob: 1.})
-            
+
         return prediction
-    
+
     def save(self, sess, model_path, global_step):
         """
         Saves the current session to a checkpoint
-        
+
         :param sess: current session
         :param model_path: path to file system location
         """
-        
+
         saver = tf.train.Saver()
         save_path = saver.save(sess, model_path, global_step=global_step)
         return save_path
-    
+
     def restore(self, sess, model_path):
         """
         Restores a session from a checkpoint
-        
+
         :param sess: current session instance
         :param model_path: path to file system checkpoint location
         """
-        
+
         saver = tf.train.Saver()
         saver.restore(sess, model_path)
         logging.info("Model restored from file: %s" % model_path)
@@ -333,43 +334,43 @@ class Ugan(object):
 class Trainer(object):
     """
     Trains a unet instance
-    
+
     :param net: the unet instance to train
     :param batch_size: size of training batch
     :param norm_grads: (optional) true if normalized gradients should be added to the summaries
     :param optimizer: (optional) name of the optimizer to use (momentum or adam)
     :param opt_kwargs: (optional) kwargs passed to the learning rate (momentum opt) and to the optimizer
-    
+
     """
-    
+
     verification_batch_size = 1
-    
+
     def __init__(self, net, batch_size=1, norm_grads=False, optimizer="momentum", opt_kwargs={}):
         self.net = net
         self.batch_size = batch_size
         self.norm_grads = norm_grads
         self.optimizer = optimizer
         self.opt_kwargs = opt_kwargs
-        
+
     def _get_optimizer(self, training_iters, global_step):
         if self.optimizer == "momentum":
             learning_rate = self.opt_kwargs.pop("learning_rate", 0.2)
             decay_rate = self.opt_kwargs.pop("decay_rate", 0.95)
             momentum = self.opt_kwargs.pop("momentum", 0.2)
-            
-            self.learning_rate_node = tf.train.exponential_decay(learning_rate=learning_rate, 
-                                                        global_step=global_step, 
-                                                        decay_steps=training_iters,  
-                                                        decay_rate=decay_rate, 
+
+            self.learning_rate_node = tf.train.exponential_decay(learning_rate=learning_rate,
+                                                        global_step=global_step,
+                                                        decay_steps=training_iters,
+                                                        decay_rate=decay_rate,
                                                         staircase=True)
-            
+
             optimizer = tf.train.MomentumOptimizer(learning_rate=self.learning_rate_node, momentum=momentum,
-                                                   **self.opt_kwargs).minimize(self.net.cost, 
+                                                   **self.opt_kwargs).minimize(self.net.cost,
                                                                                 global_step=self.global_step)
         elif self.optimizer == "adam":
             learning_rate = self.opt_kwargs.pop("learning_rate", 0.001)
             self.learning_rate_node = tf.Variable(learning_rate)
-            
+
             g_optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate_node,
                                                **self.opt_kwargs).minimize(self.net.generator_cost,
                                                                            global_step=self.global_step,
@@ -377,30 +378,30 @@ class Trainer(object):
             d_optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate_node,
                                                **self.opt_kwargs).minimize(self.net.discriminator_cost,
                                                                            var_list=self.net.discriminator_variables)
-        
+
         return g_optimizer, d_optimizer
-        
+
     def _initialize(self, training_iters, output_path, restore, prediction_path):
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
 
         self.g_optimizer, self.d_optimizer = self._get_optimizer(training_iters, self.global_step)
 
         init = tf.global_variables_initializer()
-        
+
         self.prediction_path = prediction_path
         abs_prediction_path = os.path.abspath(self.prediction_path)
         output_path = os.path.abspath(output_path)
-        
+
         if not restore:
             logging.info("Removing '{:}'".format(abs_prediction_path))
             shutil.rmtree(abs_prediction_path, ignore_errors=True)
             logging.info("Removing '{:}'".format(output_path))
             shutil.rmtree(output_path, ignore_errors=True)
-        
+
         if not os.path.exists(abs_prediction_path):
             logging.info("Allocating '{:}'".format(abs_prediction_path))
             os.makedirs(abs_prediction_path)
-        
+
         if not os.path.exists(output_path):
             logging.info("Allocating '{:}'".format(output_path))
             os.makedirs(output_path)
@@ -433,29 +434,29 @@ class Trainer(object):
               predict_step=50, restore=False, write_graph=False, prediction_path = 'prediction'):
         """
         Lauches the training process
-        
+
         :param data_provider: callable returning training and verification data
         :param output_path: path where to store checkpoints
         :param training_iters: number of training mini batch iteration
         :param epochs: number of epochs
         :param dropout: dropout probability
         :param display_step: number of steps till outputting stats
-        :param restore: Flag if previous model should be restored 
+        :param restore: Flag if previous model should be restored
         :param write_graph: Flag if the computation graph should be written as protobuf file to the output path
         :param prediction_path: path where to save predictions on each epoch
         """
         save_path = os.path.join(os.path.join(output_path, 'model'), "model.cpkt")
         if epochs == 0:
             return save_path
-        
+
         init = self._initialize(training_iters, output_path, restore, prediction_path)
-        
+
         with tf.Session() as sess:
             if write_graph:
                 tf.train.write_graph(sess.graph_def, output_path, "graph.pb", False)
-            
+
             sess.run(init)
-            
+
             if restore:
                 ckpt = tf.train.get_checkpoint_state(os.path.join(output_path, 'model'))
                 if ckpt and ckpt.model_checkpoint_path:
@@ -469,14 +470,14 @@ class Trainer(object):
             pred_shape = self.store_prediction(sess, eval_iters, eval_data_provider, border_size,
                                                patch_size, input_size, "_init", combine=True)
 
-            
+
             summary_writer = tf.summary.FileWriter(os.path.join(output_path, 'train'), graph=sess.graph)
             eval_summary_writer=tf.summary.FileWriter(os.path.join(output_path, 'eval'))
             logging.info("Start optimization")
             curr_step=tf.train.global_step(sess, self.global_step)
             curr_epoch=curr_step//(training_iters*patch_len)
 
-            epoch_tags = ['generator_cost', 'bce_loss']
+            epoch_tags = ['generator_cost', 'generator_fake_cost', 'bce_loss']
             discriminator_tags = ['discriminator_cost', 'fake_prob', 'real_prob']
             eval_tags = ['accuracy', 'precision', 'recall', 'f1', 'tp', 'fp', 'fn']
             display_tags = epoch_tags + eval_tags
