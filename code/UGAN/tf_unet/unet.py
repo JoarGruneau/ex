a7faa872a888762838d72ce23e58b393aed98e37
Joar Gruneau <joar@gruneau.se>
2018-03-24 18:53:34 +0100

    small network test update 2

--- code/UGAN/tf_unet/unet.py
+++ code/UGAN/tf_unet/unet.py
@@ -499,12 +499,14 @@ class Trainer(object):
                 if epoch%predict_step == 0:
                     self.store_prediction(sess, eval_iters, eval_data_provider,  border_size,
                                           patch_size, input_size, "epoch_%s"%epoch, combine=True)
-                d_cost = float('inf')
-                while d_cost > cut_off:
-                    d_results=self.eval_epoch(sess, data_provider, 1, [self.d_optimizer],
-                                              discriminator_tags, feed_dict)
-                    d_cost=d_results[0]
-                    self.write_logg(['type'] + discriminator_tags, ['training discriminator'] + d_results)
+
+                if epoch % check_discriminator == 0:
+                    d_cost = float('inf')
+                    while d_cost > cut_off:
+                        d_results=self.eval_epoch(sess, data_provider, 1, [self.d_optimizer],
+                                                  discriminator_tags, feed_dict)
+                        d_cost=d_results[0]
+                        self.write_logg(['type'] + discriminator_tags, ['training discriminator'] + d_results)
 
 
             logging.info("Optimization Finished!")
