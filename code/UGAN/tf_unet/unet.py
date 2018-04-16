972d8599c79fcd7051f650ece8de7a8077afaf44
Joar Gruneau <joar@gruneau.se>
2018-04-16 18:33:43 +0200

    avg pool

--- code/UGAN/tf_unet/unet.py
+++ code/UGAN/tf_unet/unet.py
@@ -67,9 +67,9 @@ def create_conv_net(x, keep_prob, channels, n_class, unet_kwargs):
             b2 = bias_variable([features])
             
             conv1 = conv2d(in_node, w1, keep_prob)
-            tmp_h_conv = tf.nn.relu(conv1 + b1)
+            tmp_h_conv = tf.nn.leaky_relu(conv1 + b1)
             conv2 = conv2d(tmp_h_conv, w2, keep_prob)
-            dw_h_convs[layer] = tf.nn.relu(conv2 + b2)
+            dw_h_convs[layer] = tf.nn.leaky_relu(conv2 + b2)
             
             size -= 4
             if layer < layers-1:
@@ -86,7 +86,7 @@ def create_conv_net(x, keep_prob, channels, n_class, unet_kwargs):
             
             wd = weight_variable_devonc([pool_size, pool_size, features//2, features], stddev)
             bd = bias_variable([features//2])
-            h_deconv = tf.nn.relu(deconv2d(in_node, wd, pool_size) + bd)
+            h_deconv = tf.nn.leaky_relu(deconv2d(in_node, wd, pool_size) + bd)
             h_deconv_concat = crop_and_concat(dw_h_convs[layer], h_deconv)
             deconv[layer] = h_deconv_concat
             
@@ -96,9 +96,9 @@ def create_conv_net(x, keep_prob, channels, n_class, unet_kwargs):
             b2 = bias_variable([features//2])
             
             conv1 = conv2d(h_deconv_concat, w1, keep_prob)
-            h_conv = tf.nn.relu(conv1 + b1)
+            h_conv = tf.nn.leaky_relu(conv1 + b1)
             conv2 = conv2d(h_conv, w2, keep_prob)
-            in_node = tf.nn.relu(conv2 + b2)
+            in_node = tf.nn.leaky_relu(conv2 + b2)
             up_h_convs[layer] = in_node
             
             size *= 2
